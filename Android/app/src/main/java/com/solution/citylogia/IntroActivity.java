368feac0a2b08f1a8dceabf52df938f50d8146ce
AndrewWeis <andreivayss@gmail.com>
2021-04-27 18:53:53 +0700

    fix

--- Android/app/src/main/java/com/solution/citylogia/IntroActivity.java
+++ Android/app/src/main/java/com/solution/citylogia/IntroActivity.java
@@ -1,17 +1,30 @@
 
 package com.solution.citylogia;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,8 +66,38 @@ public class IntroActivity extends AppCompatActivity {
                 if (introBoardingPager.getCurrentItem() + 1 < introBoardingAdapter.getItemCount()) {
                     introBoardingPager.setCurrentItem(introBoardingPager.getCurrentItem()+1);
                 } else {
-                    startActivity(new Intent(getApplicationContext(), MapActivity.class));
-                    finish();
+                    Dexter.withActivity(IntroActivity.this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                            startActivity(new Intent(IntroActivity.this, MapActivity.class));
+                            finish();
+                        }
+
+
+                        @Override
+                        public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                            if (permissionDeniedResponse.isPermanentlyDenied()) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(IntroActivity.this);
+                                builder.setTitle("Permission denied").setMessage("Permission to access device location is permanently denied")
+                                        .setNegativeButton("Cancel",null)
+                                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                Intent intent = new Intent();
+                                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                intent.setData(Uri.fromParts("package", getPackageName(), null));
+                                            }
+                                        }).show();
+                            } else {
+                                Toast.makeText(IntroActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                            permissionToken.continuePermissionRequest();
+                        }
+                    }).check();
                 }
             }
         });
@@ -102,5 +145,7 @@ public class IntroActivity extends AppCompatActivity {
         } else {
             buttonAction.setText("Свайпай");
         }
+
+
     }
 }
