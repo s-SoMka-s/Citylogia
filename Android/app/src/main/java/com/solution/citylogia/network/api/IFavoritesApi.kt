135cbaae606d37dfa623fa5c6b8a66b5cda5d08b
s-SoMka-s <S.O.M.K.A@yandex.ru>
2021-04-27 18:36:37 +0700

    Bind favorites.

--- Android/app/src/main/java/com/solution/citylogia/network/api/IFavoritesApi.kt
+++ Android/app/src/main/java/com/solution/citylogia/network/api/IFavoritesApi.kt
@@ -1,10 +1,14 @@
 package com.solution.citylogia.network.api
 
+import com.google.android.gms.location.Geofence
 import com.solution.citylogia.models.*
 import io.reactivex.Single
 import retrofit2.http.*
 
 interface IFavoritesApi {
+    @GET("Favorites")
+    fun getFavorites(): Single<BaseCollectionResponse<Favorite>>
+
     @POST("Favorites")
     fun makeFavorite(@Body data: Any): Single<BaseObjectResponse<Boolean>>
 
